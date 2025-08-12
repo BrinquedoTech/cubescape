5fd0eb737a7eacddf03b64489947a90224919e13
Michael <michael_cesar_sv@hotmail.com>
2025-08-11 23:45:25 -0300

    chore: set base and outDir for dashboard integration

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,18 @@ import glsl from 'vite-plugin-glsl';
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
-    publicDir: './public/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
-    },
-    build:
-    {
-        outDir: './dist',
-        emptyOutDir: true,
-        sourcemap: true,
-    },
-    plugins: [glsl()],
+  publicDir: './public/',
+  base: '/games/cubescape/',
+  server:
+  {
+    host: true,
+    open: !isCodeSandbox // Open if it's not a CodeSandbox
+  },
+  build:
+  {
+    outDir: '../../public/games/cubescape',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  plugins: [glsl()],
 }
